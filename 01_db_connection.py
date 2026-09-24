60508bbefadbf5670a7ee5ba373e2c8804301224
Jash Nilesh Patil <140410152+Jashpatil27@users.noreply.github.com>
2026-09-24 20:48:57 +0530

    Update 01_db_connection.py

--- 01_db_connection.py
+++ 01_db_connection.py
@@ -28,7 +28,7 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "retail_inventory")
 DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Jash_tanish@2004")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 
 
 def get_engine():
